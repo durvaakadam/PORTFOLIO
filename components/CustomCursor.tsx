7b9bc80ed99d7b1b9179ad8d88eddd44f315eac3
durvaakadam <durvakadam204@gmail.com>
2026-01-26 01:39:25 +0530

    cursor

--- components/CustomCursor.tsx
+++ components/CustomCursor.tsx
@@ -1,26 +1,29 @@
 "use client"
 
 import { useEffect, useState } from "react"
+import { useIsMobile } from "@/hooks/use-mobile"
 
 export default function CustomCursor() {
   const [pos, setPos] = useState({ x: 0, y: 0 })
+  const isMobile = useIsMobile();
 
   useEffect(() => {
+    if (isMobile) return;
     const move = (e: MouseEvent) => {
       setPos({ x: e.clientX, y: e.clientY })
     }
     window.addEventListener("mousemove", move)
     return () => window.removeEventListener("mousemove", move)
-  }, [])
+  }, [isMobile])
+  if (isMobile) return null;
 
   return (
     <div
       className="pointer-events-none fixed top-0 left-0 z-[9999]"
       style={{
         transform: `translate(${pos.x - 12}px, ${pos.y - 12}px)`,
-        // transition: 'transform 0.15s cubic-bezier(0.25, 0.46, 0.45, 0.94)'
         transition: 'transform 0.0s cubic-bezier(0.25, 0.46, 0.45, 0.94)'
-    }}
+      }}
     >
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
@@ -39,4 +42,3 @@ export default function CustomCursor() {
     </div>
   )
 }
-2
